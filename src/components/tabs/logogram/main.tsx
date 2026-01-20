14febe9257d416881b3b2661239ad4ca778f7dba
guyokay <guyokay@gmail.com>
2026-01-20 09:39:49 -0800

    (etymolog) resolve keyboard popup not showing properly (AI)

--- src/components/tabs/logogram/main.tsx
+++ src/components/tabs/logogram/main.tsx
@@ -1,6 +1,8 @@
 import {SmartForm, useSmartForm} from "smart-form/smartForm";
 import LabelShiftTextInput from "smart-form/input/fancy/redditStyle/labelShiftTextInput/labelShiftTextInput.tsx";
-import LabelShiftTextCustomKeyboardInput, {IPA_CHARACTERS} from "smart-form/input/fancy/redditStyle/labelShiftTextCustomKeyboardInput";
+import LabelShiftTextCustomKeyboardInput, {
+    IPA_CHARACTERS
+} from "smart-form/input/fancy/redditStyle/labelShiftTextCustomKeyboardInput";
 import SvgDrawerInput from "smart-form/input/basic/svgDrawerInput/svgDrawerInput.tsx";
 import HoverToolTip from "cyber-components/interactable/information/hoverToolTip/hoverToolTip.tsx";
 import classNames from "classnames";
@@ -12,27 +14,29 @@ export default function LogogramMain() {
     return (
         <SmartForm {...registerForm("logogramForm")}>
             <div className={classNames(flex.flexCol, flex.flexGapM)}>
-                {/* Script Name Field */}
-                <HoverToolTip
-                    className={classNames(sizing.parentWidth)}
-                    content={"The name or identifier for this script/logogram"}
-                >
-                    <LabelShiftTextInput
-                        displayName={"Name"}
-                        {...registerField("logogramName", {})}
-                    />
-                </HoverToolTip>
+                <div>
+                    {/* SVG Drawing Canvas */}
+                    <HoverToolTip
+                        className={sizing.fitContent}
+                        content={"Draw your script character or logogram here"}
+                    >
+                        <SvgDrawerInput
+                            displayName={"Script Drawing"}
+                            {...registerField("logogramSvg", {})}
+                        />
+                    </HoverToolTip>
 
-                {/* SVG Drawing Canvas */}
-                <HoverToolTip
-                    className={sizing.fitContent}
-                    content={"Draw your script character or logogram here"}
-                >
-                    <SvgDrawerInput
-                        displayName={"Script Drawing"}
-                        {...registerField("logogramSvg", {})}
-                    />
-                </HoverToolTip>
+                    {/* Script Name Field */}
+                    <HoverToolTip
+                        className={classNames(sizing.parentWidth)}
+                        content={"The name or identifier for this script/logogram"}
+                    >
+                        <LabelShiftTextInput
+                            displayName={"Name"}
+                            {...registerField("logogramName", {})}
+                        />
+                    </HoverToolTip>
+                </div>
 
                 {/* Pronunciation Field with IPA Keyboard */}
                 <HoverToolTip
