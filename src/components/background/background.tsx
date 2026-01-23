2be921d0d039202c89a7797a6a2c8ab8d05e7b89
guyokay <guyokay@gmail.com>
2026-01-22 23:01:18 -0800

    (cyber-comp) resolve backgroundComponent styling issue

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -18,11 +18,19 @@ export default function Background({
                 sizing.parentWidth,
                 sizing.paddingL,
                 flex.flexColumn,
-                flex.flexGapM,
-                flex.justifyContentCenter
+                flex.alignItemsCenter
             )}
+            style={{
+                minHeight: '100dvh',
+                minWidth: '100%'
+            }}
         >
+            <div
+                className={classNames(flex.flexColumn, flex.flexGapM)}
+                style={{ maxWidth: '1200px', width: '100%' }}
+            >
                 {children}
+            </div>
         </BackgroundComponent>
     )
 }
